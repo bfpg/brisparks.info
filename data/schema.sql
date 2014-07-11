cf02ef6d6e92b952ef7d87aade998df89ab4bfde
Ben Kolera <ben.kolera@gmail.com>
2014-07-11 23:35:05 +1000

    First go at the schema for facilities.

---
+++ data/schema.sql
@@ -0,0 +1,39 @@
+CREATE EXTENSION postgis;
+CREATE EXTENSION postgis_topology;
+CREATE EXTENSION fuzzystrmatch;
+CREATE EXTENSION postgis_tiger_geocoder;
+
+DROP TABLE park_facility;
+   
+CREATE TABLE park_facility (
+  park_number   VARCHAR(20)
+  , park_name   VARCHAR(20)
+  , node_id     INTEGER
+  , node_use    VARCHAR(100)
+  , node_name   VARCHAR(100)
+  , item_id     VARCHAR(20)
+  , item_type   VARCHAR(20)
+  , item_name   VARCHAR(20)
+  , description VARCHAR(255) 
+  , easting     NUMERIC(19,10)
+  , northing    NUMERIC(19,10)
+  , orig_fid    INTEGER
+  , coords      GEOGRAPHY(Point)
+);
+
+
+INSERT INTO park_facility VALUES (
+  'D0001'
+  , 'QUANDONG PARK'
+  , '32423'
+  , 'RECREATION ACCESS'
+  , 'QUANDONG PK SHARED PATHWAY'
+  , '11882'
+  , 'ADVISORY SIGN'
+  , ''
+  , 'Shared pathway sign'
+  , '500106.05'
+  , '6965225.40'
+  , 1 
+  , 'POINT(153.0010731 -27.43642293)'
+);
